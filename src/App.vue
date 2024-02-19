87430380d8bc3c621e5cc13f85275c664724559b
agnesvarga1 <agikolyok@gmail.com>
2024-02-19 12:35:45 +0100

    1st commit set up workspace

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <script>
+import AppHeader from "./components/AppHeader.vue";
 export default {
   name: "App",
+  components: {
+    AppHeader,
+  },
 };
 </script>
 
-<template></template>
+<template>
+  <AppHeader />
+  <h1>Hello</h1>
+</template>
 
 <style lang="scss">
 @use "./styles/general.scss";
