788934a22e724a4fa97b63aa38f5acb148b5d2aa
agnesvarga1 <agikolyok@gmail.com>
2024-05-17 11:05:26 +0200

    fix countdown section nav bar responsive init

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,6 +9,7 @@ export default {
       end: false,
       isScrolled: false,
       scrollPos: 0,
+      isMenuOpen: false,
     };
   },
 
@@ -16,15 +17,12 @@ export default {
     timer() {
       let nowInMs = new Date().getTime();
 
-      let endDateInMs = new Date("May 14, 2024 19:40:00").getTime();
+      let endDateInMs = new Date("May 17, 2024 23:40:00").getTime();
 
       let diff = endDateInMs - nowInMs;
 
       let diffInS = diff / 1000;
-      if (diffInS === 0) {
-        end = true;
-        return;
-      }
+
       let mTotal = Math.floor(diffInS / 60);
 
       let h = Math.floor(mTotal / 60);
@@ -34,9 +32,9 @@ export default {
       let m = Math.floor(mTotal - h * 60);
       let s = Math.floor(diffInS - mTotal * 60);
 
-      if (d === 0 && hdis === 0 && m === 0 && s === 0) {
-        end = true;
-        clearInterval(countDown);
+      if (diff <= 0) {
+        this.end = true;
+        clearInterval(this.countDown);
       } else {
         hdis < 10 ? (hdis = `0${hdis}`) : hdis;
         d < 10 ? (d = `0${d}`) : d;
@@ -47,7 +45,7 @@ export default {
       }
     },
     countDown() {
-      this.countDowEnd = setInterval(this.timer, 1000);
+      this.countDownEnd = setInterval(this.timer, 1000);
     },
     handleScroll(event) {
       if (window.scrollY > 50) {
@@ -55,12 +53,15 @@ export default {
       } else if (window.scrollY < 50) {
         this.isScrolled = false;
       }
-      console.log(this.scrollPos);
+    },
+    toggleMenu() {
+      this.isMenuOpen = true;
     },
   },
   mounted() {
     this.timer();
     this.countDown();
+
     window.addEventListener("scroll", this.handleScroll);
   },
   beforeUnmount() {
@@ -70,11 +71,8 @@ export default {
 </script>
 <template>
   <header>
-    <div class="wrapper">
-      <div
-        v-show="end === false || isScrolled === false"
-        class="count-down-container"
-      >
+    <div v-show="isScrolled === false && end === false" class="wrapper">
+      <div class="count-down-container">
         <h4>Starts TOMORROW! Our biggest event of the year...</h4>
         <div>
           <div class="display-clock">
@@ -88,6 +86,7 @@ export default {
 
     <div class="nav-bar" :class="isScrolled === true ? 'sticky-nav' : ''">
       <figure><img src="../assets/img/dark-logo.png" alt="logo" /></figure>
+
       <!-- NAV BAR LINKS -->
       <ul class="nav-bar-links">
         <li>
@@ -124,14 +123,63 @@ export default {
           <a href="#"><i class="fa-brands fa-linkedin"></i></a>
         </li>
       </ul>
+      <div class="nav-modal" :class="isMenuOpen ? 'visible' : ''">
+        <ul class="links-sm">
+          <li>
+            <a href="#">Home<i class="fa-solid fa-angle-down"></i></a>
+          </li>
+          <li>
+            <a href="#">Pages<i class="fa-solid fa-angle-down"></i></a>
+          </li>
+          <li>
+            <a href="#">courses<i class="fa-solid fa-angle-down"></i></a>
+          </li>
+          <li>
+            <a href="#">Features<i class="fa-solid fa-angle-down"></i></a>
+          </li>
+          <li>
+            <a href="#">Blog<i class="fa-solid fa-angle-down"></i></a>
+          </li>
+          <li>
+            <a href="#">shop<i class="fa-solid fa-angle-down"></i></a>
+          </li>
+        </ul>
+        <!-- SOCIAL LINKS -->
+        <ul class="social-sm">
+          <li>
+            <a href="#"><i class="fa-brands fa-x-twitter"></i></a>
+          </li>
+          <li>
+            <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+          </li>
+          <li>
+            <a href="#"><i class="fa-brands fa-instagram"></i></a>
+          </li>
+          <li>
+            <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+          </li>
+        </ul>
+      </div>
+      <div
+        @click="toggleMenu()"
+        v-show="isMenuOpen === false"
+        class="menu-icon-container"
+      >
+        <div class="bar"></div>
+        <div class="bar"></div>
+      </div>
     </div>
   </header>
 </template>
 <style lang="scss" scoped>
 @use "../styles/partials/variables" as *;
 @use "../styles/partials/mixins" as *;
+
 header {
   position: relative;
+  .nav-modal {
+    display: none;
+  }
   .wrapper {
     background-color: $white-vari;
   }
@@ -143,7 +191,7 @@ header {
     align-items: center;
     gap: 1.5rem;
     padding-block: 1rem;
-    font-size: 1.3rem;
+    font-size: 1rem;
     h4 {
       opacity: 0.7;
       font-weight: 400;
@@ -217,6 +265,36 @@ header {
     top: 0;
     width: 100%;
     padding-inline: 5%;
+  }
+}
+
+@media only screen and (max-width: 992px) {
+  .nav-bar ul.nav-bar-links,
+  .nav-bar .socials {
+    display: none;
+  }
+  .nav-modal {
+    position: absolute;
+    top: 0;
+    right: 0;
+    z-index: 20;
+    .links-sm {
+      display: flex;
+    }
+  }
+
+  .menu-icon-container {
+    display: block;
+    width: 5%;
+    .bar {
+      width: 80%;
+      height: 1px;
+      background: #000;
+      margin: 5px auto;
+    }
+  }
+  .visible {
+    display: block;
   }
 }
 </style>
